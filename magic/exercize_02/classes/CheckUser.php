1192cfd0c43cd4c3567ac121b30cbfe2f63dcc22
SerVin <v.s.work@yandex.ru>
2026-05-12 19:31:40 +0300

    added folder magic

---
+++ magic/exercize_02/classes/CheckUser.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+class CheckUser
+{
+    use AppUserAuthentication, MobileUserAuthentication {
+        AppUserAuthentication::authenticate insteadof MobileUserAuthentication;
+
+        AppUserAuthentication::authenticate as authenticate;
+        MobileUserAuthentication::authenticate as mAuthenticate;
+    }
+
+    // Метод авторизации
+    public function login($login, $password)
+    {
+
+        echo "Попытка входа с логином: $login\n";
+
+        // авторизация для App.
+        $result = $this->authenticate($login, $password);
+        if ($result) return $result;
+
+        // авторизация для Mob.
+        $result = $this->mAuthenticate($login, $password);
+        if ($result) return $result;
+
+        return "Авторизация не удалась: Неверный логин или пароль";
+    }
+}
---
+++ magic/exercize_03/traits/MobileUserAuthentication.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+trait MobileUserAuthentication
+{
+    private string $mLogin = "user_M1";
+    private int $mPassword = 1234;
+
+    public function authenticate(string $login, int $password): string | bool
+    {
+        if ($this->mLogin === $login && $this->mPassword === $password) {
+            return "пользователь с логином: '$login' авторизован как пользователь мобильного приложения";
+        } else {
+            return false;
+        }
+    }
+}
